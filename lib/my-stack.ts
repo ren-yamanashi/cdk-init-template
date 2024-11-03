08619025248d55c53777dc414d6550b5c40b447f
ren-yamanashi <pinokionigg@icloud.com>
2024-11-03 17:15:12 +0900

    chore: add Biome

--- lib/my-stack.ts
+++ lib/my-stack.ts
@@ -1,5 +1,5 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
 export class MyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
